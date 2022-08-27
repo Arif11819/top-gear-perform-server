cfee3eb503bde9ac198ca209707f009ea3a383d6
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-08-27 22:14:29 +0600

    added

--- index.js
+++ index.js
@@ -555,6 +555,13 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ userEmail: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        });
+
 
 
     }
