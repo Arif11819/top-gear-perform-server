39e34a60780a6145a1bb60319d8e0805cbb81476
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-08-27 12:33:59 +0600

    added

--- index.js
+++ index.js
@@ -407,7 +407,7 @@ async function run() {
 
 
 
-        //============== Mazharul ===================
+        //============== Mazharul Vai ===================
         //post schedule data
         app.post('/scheduleData', async (req, res) => {
             const newScheduleData = req.body;
